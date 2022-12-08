fbcc5dbeac1133be55cfd5ffa6bd38815ef76a1f
jnicoll12 <jnicoll.rl@gmail.com>
2022-12-08 01:16:21 -0500

    Configuracion de template y archivos estaticos

--- jhonn/views.py
+++ jhonn/views.py
@@ -1,4 +1,30 @@
 from django.shortcuts import render
+from .models import Home, About, Profile, Category, Skills, Portfolio
 
 def index(request):
-    return render(request, 'index.html')
+    # Incio
+    home = Home.objects.latest('updated')
+
+    # Acerca de
+        #Obteno el ultimo regitro
+    about = About.objects.latest('updated')
+        #Que me extraiga el ultimo perfil registrado
+    profiles = Profile.objects.filter(about=about)
+
+    # Habilidades
+    categories = Category.objects.all()
+
+    # Portafolio
+    portfolios = Portfolio.objects.all()
+
+    context={
+        'home':home,
+        'about':about,
+        'profiles':profiles,
+        'categories': categories,
+        'portfolios': portfolios,
+    }
+
+    
+
+    return render(request, 'index.html', context)
